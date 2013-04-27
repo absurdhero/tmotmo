654c5ea089751bdb7eef98c2d01d0d1afbba5283
Brandon Bremen <bbremen@gmail.com>
2013-04-27 10:49:48 -0700

    Fixed camera tests

--- tests-mac/CameraTests.cs
+++ tests-mac/CameraTests.cs
@@ -6,74 +6,49 @@ using NUnit.Framework;
 [TestFixture]
 public class CameraTests
 {
-	static Viewport iPhoneViewport ()
-	{
-		Viewport viewport = new Viewport ();
-		viewport.Height = 320;
-		viewport.Width = 480;
-		viewport.MaxDepth = 1.0f;
-		viewport.MinDepth = 0.0f;
-		return viewport;
+	static Viewport iPhoneViewport {
+		get {
+			Viewport viewport = new Viewport ();
+			viewport.Height = 320;
+			viewport.Width = 480;
+			viewport.MaxDepth = 1.0f;
+			viewport.MinDepth = 0.0f;
+			return viewport;
+		}
 	}
 
+	float orthographicSize = 160;
 	Vector2 iPhoneDimensions = new Vector2 (480, 320);
+	Camera camera;
+
+	[SetUp]
+	public void createCamera() {
+		 camera = new Camera (iPhoneViewport);
+	}
 
 	[Test]
 	public void originIsAtCenterOfScreen()
 	{
-		var viewport = iPhoneViewport();
-		Camera camera = new Camera(viewport);
-		
 		Assert.That (camera.WorldToScreen2D(Vector2.Zero), Is.EqualTo(iPhoneDimensions / 2));
 	}
 
 	[Test]
 	public void topOfScreenIs100UnitsFromMiddle()
 	{
-		var viewport = iPhoneViewport();
-		Camera camera = new Camera(viewport);
-		
-		Assert.That (camera.WorldToScreen2D(new Vector2(100 * viewport.AspectRatio, 100)), Is.EqualTo(iPhoneDimensions));
+		Assert.That (camera.WorldToScreen2D(new Vector2(orthographicSize * iPhoneViewport.AspectRatio, orthographicSize)), Is.EqualTo(iPhoneDimensions));
 	}
 
 	[Test]
 	public void centerPixelIsAtOrigin()
 	{
-		var viewport = iPhoneViewport();
-		Camera camera = new Camera(viewport);
-		
 		Assert.That (camera.ScreenToWorld2D(iPhoneDimensions / 2), Is.EqualTo(Vector2.Zero));
 	}
 
 	[Test]
 	public void centerOfViewportIsAtOrigin()
 	{
-		var viewport = iPhoneViewport();
-		Camera camera = new Camera(viewport);
-		
 		Assert.That (camera.ViewportToWorld2D(new Vector2(0.5f, 0.5f)), Is.EqualTo(Vector2.Zero));
-		Assert.That (camera.ViewportToWorld2D(new Vector2(0.5f, 0f)), Is.EqualTo(new Vector2(0, -100)));
-	}
-
-	[Test]
-	public void cameraDoesThings ()
-	{
-		var viewport = iPhoneViewport();
-		Camera camera = new Camera(viewport);
-
-		var center = new Vector2(40, 10) + camera.WorldToScreen2D(Vector2.Zero);
-
-		System.Console.WriteLine ("s0,0 : " + camera.ScreenToWorld2D(Vector2.Zero));
-		System.Console.WriteLine ("v1,1 : " + camera.ViewportToWorld2D(Vector2.One));
-		System.Console.WriteLine ("wcenter : " + camera.ScreenToWorld2D(new Vector2(center.X, center.Y)));
-
-		var layoutpos = camera.ViewportToWorld2D(new Vector2(0.5f, 0.65f));
-		System.Console.WriteLine ("screenEdgesToWorld " + camera.ScreenToWorld2D(Vector2.Zero) + ", " + camera.ScreenToWorld2D(Vector2.One));
-		System.Console.WriteLine ("viewToworld(0.5f, 0.65f) => " + layoutpos);
-		System.Console.WriteLine ("viewToScreen => " + camera.WorldToScreenPoint(new Vector3(layoutpos, Camera.NEAR)));
-		System.Console.WriteLine ("minus center viewToworld(0.5f, 0.65f) => " + layoutpos);
-		System.Console.WriteLine ("minus center viewToScreen(0.5f, 0.65f) => " + camera.WorldToScreenPoint(new Vector3(layoutpos, Camera.NEAR)));
-
+		Assert.That (camera.ViewportToWorld2D(new Vector2(0.5f, 0f)), Is.EqualTo(new Vector2(0, orthographicSize)));
 	}
 }
 
--- tests-mac/TransformTest.cs
+++ tests-mac/TransformTest.cs
@@ -4,17 +4,25 @@ using Microsoft.Xna.Framework;
 
 namespace tests
 {
-    [TestFixture()]
+    [TestFixture]
     public class TransformTest
     {
-        [Test()]
-        public void localMatrixIsSameAsWorldMatrixWhenParentUnset()
+        [Test]
+        public void localMatrixAndWorldMatrixAreIdentityWhenNew()
         {
             Transform transform = new Transform();
-            Assert.That(transform.localMatrix, Is.EqualTo(transform.worldMatrix));
+            Assert.That(transform.localMatrix, Is.EqualTo(Matrix.Identity));
+			Assert.That(transform.worldMatrix, Is.EqualTo(Matrix.Identity));
+		}
 
-            var expectedTranslation = Vector3.Up;
-            transform.localTranslation = expectedTranslation;
+		[Test]
+		public void localAndWorldTranslationAreEqualWhenParentUnset()
+		{
+			Transform transform = new Transform();
+            var anyTranslation = Vector3.Up;
+
+            transform.localTranslation = anyTranslation;
+			Assert.That (transform.Translation, Is.EqualTo(anyTranslation));
             Assert.That(transform.localMatrix, Is.EqualTo(transform.worldMatrix));
         }
     }
